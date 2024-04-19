e479ed5c77093e6101f0c2aca9da60574942b2ec
Ben Cavins <ben.cavins@flatironschool.com>
2024-04-19 10:37:33 -0400

    show answers in clue bank

--- frontend/crossbuild/src/components/Clue.jsx
+++ frontend/crossbuild/src/components/Clue.jsx
@@ -1,5 +1,6 @@
 export default function Clue({ num, text, answer, length }) {
   return <div className="clue">
     <p>{num}. {text} ({length} letters)</p>
+    <p>Answer: {answer}</p>
   </div>
 }
--- frontend/crossbuild/src/components/ClueList.jsx
+++ frontend/crossbuild/src/components/ClueList.jsx
@@ -5,7 +5,12 @@ export default function ClueList({ clues }) {
 //   console.log(Object.entries(clues).map(([num, data]) => console.log(data)))
   return <div className="clue-list">
     {Object.entries(clues).map(([num, data]) => {
-      return <Clue key={num} num={num} text={data.text} length={data.length} />
+      return <Clue 
+        key={num} 
+        num={num} 
+        text={data.text} 
+        length={data.length} 
+        answer={data.answer} />
     })}
   </div>
 }
